05b0b7b84a8217b431a0c5aa121ed1fbf6dd27b0
Gero Mendy <geromendy@gmail.com>
2020-10-15 19:11:43 -0300

    eliminado listener('load') por fallo en GHPages

--- js/entregable03/main.js
+++ js/entregable03/main.js
@@ -31,9 +31,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let hero_parallax = document.querySelector("#hero_parallax_principal");
     let parallax_layers = document.querySelectorAll(".parallax_layer");
-    parallax_layers[parallax_layers.length-1].addEventListener("load",()=>{
-        showLoading(0);
-    });
+    // parallax_layers[parallax_layers.length-1].addEventListener("load",()=>{
+    //     showLoading(0);
+    // });
 
     let hero_parallax_controller = new HeroParallax(parallax_layers, hero_parallax_change_values);
 
@@ -182,7 +182,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     /* Fin carrousel */
 
-    // showLoading(/* Debug */0);
+    showLoading(3000);
 
     window.addEventListener("resize", ajustarHeightParallax);
 
